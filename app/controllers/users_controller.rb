cebb9d5953004c07bebe8961c1ba9914578dd20f
Anu Patil <anu@anupatil.com>
2013-04-04 20:53:44 -0700

    GA Lession 12 - Updating using controller, views and twitter goodies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,39 @@
 class UsersController < ApplicationController
   def index
+    #@users = User.all
+    @users = User.order(:username).all
   end
 
   def show
+    begin
+      @user = User.find params[:id]
+
+    rescue
+      redirect_to users_path, :alert => "No user by that ID."
+    end
   end
 
   def edit
+    begin
+      @user = User.find params[:id]
+      rescue
+        redirect_to users_path :alert => "No user by that ID."
+      end
   end
 
   def update
+    begin
+    @user = User.find_by_id(params[:id])
+    if @user.update_attributes(params[:user])
+    redirect_to @user, notice: "User was saved successfully"
+    else 
+      render :edit
+    end
+
+  rescue
+    render :edit
   end
+end
 
   def new
     @user = User.new #instance variable that says when we create a new user it will be submitted to the user database model
@@ -17,7 +41,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new (params[:user])
-    @user.save!
+    if @user.valid? then
+      if @user.save!
+        redirect_to @user, notice: "User was saved successfully"
+      else 
+        render :new
+      end
+    else 
+      render :new
+    end
   end
 
   def destroy
